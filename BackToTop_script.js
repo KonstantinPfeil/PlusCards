32d6466408fc3f1ab3999bc5d8880293e38d280f
KonstantinPfeil <pfeilkonstantin@gmail.com>
2022-06-26 00:33:06 +0200

    BackToTop

---
+++ BackToTop_script.js
@@ -0,0 +1,20 @@
+let calcScrollValue = () => {
+    let scrollProgress = document.getElementById("progress");
+    let progressValue = document.getElementById("progress-value");
+    let pos = document.documentElement.scrollTop;
+    let calcHeight =
+      document.documentElement.scrollHeight -
+      document.documentElement.clientHeight;
+    let scrollValue = Math.round((pos * 100) / calcHeight);
+    if (pos > 100) {
+      scrollProgress.style.display = "grid";
+    } else {
+      scrollProgress.style.display = "none";
+    }
+    scrollProgress.addEventListener("click", () => {
+      document.documentElement.scrollTop = 0;
+    });
+    scrollProgress.style.background = `conic-gradient(rgb(96,194,120) ${scrollValue}%, rgba(75, 130, 133, 0.75) ${scrollValue}%)`;
+  };
+  window.onscroll = calcScrollValue;
+  window.onload = calcScrollValue;
